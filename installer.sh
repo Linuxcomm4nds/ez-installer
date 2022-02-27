519ae5160d2e00350c1ff167900241a3e1bf6608
Linuxcomm4nds <100486124+Linuxcomm4nds@users.noreply.github.com>
2022-02-27 12:00:53 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -18,6 +18,38 @@ then
 	echo '';
 	echo '';
 	echo '';
+	if ! command -v nmap &> /dev/null 
+	then 
+        	read -p "Do you wanna install Nmap if not some commands wont work?(y/n)" -n 1 -r
+        	if [[ $REPLY =~ ^[Yy]$ ]]
+        	then
+                	sudo apt install nmap
+                	echo '';
+        	fi
+        
+	fi
+	if ! command -v anonsurf &> /dev/null 
+	then 
+        	read -p "Do you wanna install Anonsurf if not you cant use the command to change your IP?(y/n)" -n 1 -r
+        	if [[ $REPLY =~ ^[Yy]$ ]]
+        	then
+                	cd $HOME;
+                	mkdir Anonsurf && cd Anonsurf
+                	git clone https://github.com/Und3rf10w/kali-anonsurf.git
+                	sudo ./installer.sh
+                	echo '';
+        	fi
+	fi
+	if ! command -v openvpn &> /dev/null 
+	then 
+        	read -p "Do you wanna install Openvpn if not you cant use the inbuild connection to Hack the box (This is only needed to connect to HTB)?(y/n)" -n 1 -r
+        	if [[ $REPLY =~ ^[Yy]$ ]]
+        	then
+        	        sudo apt install openvpn
+                	echo '';
+        	fi
+	fi
+
 	echo 'DONE ! Thanks for using Ez commands';
 echo '###########################			###########################';
 echo '###########################			###########################';
